1114a49df8f1f797dca8e948611cecfe44d0f929
Edione sousa <117118716+Edionesousa@users.noreply.github.com>
2023-05-05 14:23:24 -0300

    estudo js

--- html/script.js
+++ html/script.js
@@ -1,3 +1,7 @@
 
+const vermelha = document.getElementById("vermelha");
+const amarela = document.getElementById("amarela");
+const verde = document.getElementById("verde");
 
+const luz = document.getElementById("luz");
 
